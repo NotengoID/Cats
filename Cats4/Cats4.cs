8f82f72da7414a2f02f37e676ee42cdbea8874e2
NotengoID <tcarrillof@gmail.com>
2017-03-15 15:59:02 -0500

    cat
    inicio

---
+++ Cats4/Cats4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Cats4 {
+    public class Cats4 : ContentPage {
+        public Cats4() {
+            var button = new Button {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+            Content = button;
+        }
+    }
+}
